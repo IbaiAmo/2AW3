fb38b78c1cbd15448d38d3dc5f0272457ceb0921
2AW3 User 2 <2AW3-2@yourdomain.com>
2024-09-12 13:55:02 +0200

    ejercicio 4 de iteraciones hecho

--- Entorno Servidor/Iteraciones/ejercicio4.php
+++ Entorno Servidor/Iteraciones/ejercicio4.php
@@ -6,17 +6,41 @@ Con While y con For
 
 $primerNum = 4;
 $ultimoNum = 9;
-
-$suma;$siguiente;
-$contador = $primerNum;
-
-while ($contador < $ultimoNum){
-    $siguiente = $contador + 1;
-    $suma = $contador + $siguiente;
-    echo "$primerNum + $siguiente = $suma <br>";
-    
-    $primerNum = $suma;
-    $contador++;
-}
-
+$numTotal = 0;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+    <p>
+        <?php
+            echo "While:<br>";
+            while($primerNum < $ultimoNum){
+                $numTotal += $primerNum;
+                $primerNum++;
+                echo $primerNum . " + " . $numTotal . " = " . ($numTotal + $primerNum) . "<br>";
+            }
+            
+        ?>
+
+    </p>
+
+    <p>
+        <?php
+            $primerNum = 4;
+            $numTotal = 0;
+            echo "For:<br>";
+
+            for($primerNum; $primerNum < $ultimoNum; $primerNum++) { 
+                $numTotal += $primerNum;
+                echo $primerNum + 1 . " + " . $numTotal . " = " . ($numTotal + $primerNum+1) . "<br>";
+            }
+             
+        ?>
+    </p>
+</body>
+</html>
