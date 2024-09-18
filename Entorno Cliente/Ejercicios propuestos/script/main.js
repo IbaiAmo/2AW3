9d86cab759175a26eea3ada8bf8f517913f837a2
2AW3 User 2 <2AW3-2@yourdomain.com>
2024-09-18 13:40:18 +0200

    Ejercicios propuestos hasta el 6, localStorage en el 5

--- Entorno Cliente/Ejercicios propuestos/script/main.js
+++ Entorno Cliente/Ejercicios propuestos/script/main.js
@@ -7,6 +7,7 @@ const $body = document.querySelector('body');
 const $template = document.getElementById("template");
 
 
+
 function ejercicio1(){
     $panelEjercicio.innerHTML = "";
     const clone = $template.content.cloneNode(true);
@@ -65,28 +66,114 @@ function ejercicio3(){
 }
 
 function ejercicio4(){
-    $panelEjercicio.innerHTML = `
+
+    const html = `
         <h3>Objeto screen</h3>
         <p>La resolución de la pantalla es ${screen.width + "x" + screen.height}</p><br>
         <h3>Objeto navigator</h3>
         <p>El nombre del navegador que usas es: ${navigator.appName}</p>
         <p>Usas el siguiente sistema operativo: ${navigator.platform}</p>
-        <p>La versión del navegador que usas es: ${navigator.appVersion}</p>
+        <p>La versión del navegador que usas es: ${navigator.appVersion}</p><br>
         <h3>Objeto document</h3>
-        <p>La URL del documento es: <a href="${document.URL}">URL</a></p>
+        <p>La URL del documento es: <a href="${document.URL}">URL</a></p><br>
         <h3>Objeto location</h3>
-        <p>El protocolo usado para acceder a esta página ha sido: ${location.protocol}</p>`;
-    let ventana = window.open("https://www.google.com", "_blank", "toolbar=yes,scrollbars=no,resizable=0,top=50,left=150,width=600,height=400");
+        <p>El protocolo usado para acceder a esta página ha sido: ${location.protocol}</p><br>
+        `;
+
+        const boton = document.createElement('button');
+        boton.classList.add('boton');
+        boton.textContent = "Abrir ventana";
+        boton.onclick = ()=>{
+            window.open("https://www.google.com", "_blank", "toolbar=0,scrollbars=0,resizable=0,top=50,left=150,width=600,height=400");
+        }
+
+    $panelEjercicio.innerHTML = html;
+    $panelEjercicio.appendChild(boton);
 }
 
 function ejercicio5(){
     $panelEjercicio.innerHTML = "";
+    const clone = $template.content.cloneNode(true);
+    const ej5 = clone.getElementById('ejercicio5');
+    const input = clone.querySelector('#ejercicio5 > div > input');
+    const boton = clone.querySelector('#ejercicio5 > div > button');
+    let texto = clone.querySelector('#ejercicio5 > #aviso');
+    let recordText = clone.querySelector('#ejercicio5 > #record');
+
+    recordText.textContent = "Tu record: " + localStorage.getItem("recordTiempo");
+
+    let startedWriting = false; //Para saber si ha empezado a escribir.
+    let fechaInicio = new Date(); //esta fecha es para luego poder cambiarla y sin errores
+
+    let cont = 0; //contador para que no se cambie la fecha cada vez que se escriba.
+    
+    input.addEventListener('input', () =>{
+        cont++;
+        startedWriting = true;
+        if(cont == 1){
+            fechaInicio = new Date();
+        }
+    });
+        
+    boton.onclick = () => {
+        calcularTiempo();
+    }
+
+    //Aparte de mostrar el resultado con el boton, también con el enter.
+    input.addEventListener('keydown', (e) =>{
+        if(e.key == "Enter"){
+            calcularTiempo();
+        }
+    });
+
+    function calcularTiempo(){
+        const fechaFin = new Date();//Fecha recogida al pulsar el boton.
+        
+        if(startedWriting){
+            const fechaFin = new Date();
+            const secs = (fechaFin - fechaInicio) /1000;
+            texto.innerHTML = "Hola " + input.value + ", has tardado " + secs + " segundos."; //dividido entre 1000 para calcular los segundos. 
+            input.value = "";
+            startedWriting = false;
+            cont = 0;
+            record(secs);
+        }else{
+            texto.innerHTML = "Empieza a escribir para calcular el tiempo";
+        }
+    }
+
+    function record(secs){
+        if (secs < localStorage.getItem("recordTiempo")){
+            localStorage.setItem("recordTiempo", secs);
+            recordText.textContent = "tu record: " + localStorage.getItem("recordTiempo");
+        }
+    }
+
+    $panelEjercicio.appendChild(ej5);
     
 }
 
 function ejercicio6(){
     $panelEjercicio.innerHTML = "";
+    const clone = $template.content.cloneNode(true);
+    const ej6 = clone.getElementById('ejercicio6');
+    const input = clone.querySelector('#ejercicio6 > div > input');
+    const boton = clone.querySelector('#ejercicio6 > div > button');
+    let texto = clone.querySelector('#ejercicio6 > p');
+
+    const fechaActual = new Date();
+    boton.onclick = () =>{
+        let birth = new Date(input.value);
+        let years = fechaActual.getFullYear() - birth.getFullYear();
+        if ((fechaActual.getDate() < birth.getDate()) && (fechaActual.getMonth() <= birth.getMonth())){
+            years--;
+        }
+        
+        texto.innerHTML = "Has nacido el " + input.value + "<br>Tienes " + years + " años.";
+    }
     
+
+    $panelEjercicio.appendChild(ej6);
 }
 
 function ejercicio7(){
