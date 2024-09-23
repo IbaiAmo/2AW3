e9ad3f2af8975329d74ebcc04e2acdb8594525f7
2AW3 User 2 <2AW3-2@yourdomain.com>
2024-09-23 13:28:50 +0200

    caballeros página con BBDD

---
+++ Entorno Servidor/mysql/caballleros/php/index.php
@@ -0,0 +1,43 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://cdn-icons-png.freepik.com/256/5305/5305216.png?semt=ais_hybrid">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Caballeros - Inicio</title>
+</head>
+<body>
+    <main>
+        <div id="title">
+            <h1>Caballeros</h1>
+            <img src="https://e7.pngegg.com/pngimages/745/211/png-clipart-paper-text-frame-brown-pattern-medieval-european-border-background-brown-wooden-f-border-texture-thumbnail.png">
+        </div>
+
+        <div id="tablaCont">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Fuerza</th>
+                        <th>Activo</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>adios</td>
+                        <td>adios</td>
+                        <td>adios</td>
+                        <td>adios</td>
+                        <td>adios</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
