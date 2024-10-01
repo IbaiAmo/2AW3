4ee640d2bda1501dc7c2cdb0507aa6ef61b04ff4
Ibitroll <ibai.amo.ballesteros@gmail.com>
2024-10-02 00:40:10 +0200

    Ejercicio 3 de DOM hecho y Menu

--- Entorno Cliente/DOM/script/main.js
+++ Entorno Cliente/DOM/script/main.js
@@ -1,9 +1,44 @@
-//elementos del DOM
+//Menu para seleccionar los ejercicios
+const menu = document.querySelector('aside');
+const cerrar = document.querySelector('aside > i');
+const body = document.querySelector('body');
+const info = document.getElementById('info');
+
+if (window.outerWidth < 600){
+    let xStart = 0;
+    let xEnd = 0;
+
+    //código para abrir y cerrar el menu
+    body.addEventListener('touchstart', (e) =>{
+        xStart = e.touches[0].clientX;
+    });
+
+    body.addEventListener('touchend', (e) =>{
+        xEnd = e.changedTouches[0].clientX;
+        const diff = xStart - xEnd;
+        console.log(diff);
+        
+
+        if(xStart < xEnd && diff * -1 > 80){
+            menu.classList.remove('showMenu');
+        }else if(xStart > xEnd && diff > 80){
+            menu.classList.add('showMenu');
+        }
+    });
+
+    cerrar.addEventListener('click', () =>{
+        menu.classList.remove('showMenu');
+    });
+}
+
+
+//Ejercicio 3
 const template = document.getElementById('template');
 const inputMensaje = document.getElementById('mensajeInput');
 const btnEnviar = document.getElementById('enviar');
 const panelMensajes = document.getElementById('mensajes');
 const inputFoto = document.getElementById('foto');
+const ej3 = document.getElementById('e3');
 
 btnEnviar.addEventListener('click', () =>{
     if (!inputMensaje.value.trim() == ""){
@@ -27,5 +62,15 @@ inputFoto.onchange = () =>{
         panelMensajes.scrollTop = panelMensajes.scrollHeight;
     }
 }
+
+function ejercicio3(){
+    info.style.display = "none";
+    menu.classList.remove('showMenu');
+    ej3.classList.remove('inActivo');
+}
+
+
+//Ejercicio 4
+
 
 
