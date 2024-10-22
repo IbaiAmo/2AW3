89797a12e6a5ffc43b11b04a5540f4dee8768e7e
2AW3 User 2 <2AW3-2@yourdomain.com>
2024-10-22 12:53:46 +0200

    Cambios en el perfil Twitter y cambio de nombre a combos anidados 2D

--- Entorno Cliente/twitter/scripts/perfil.js
+++ Entorno Cliente/twitter/scripts/perfil.js
@@ -1,5 +1,11 @@
 const cerrarSesionBtn = document.getElementById('btnCerrarSesion');
 
+const usuariosArray2 = JSON.parse(localStorage.getItem("usuarios"));
+
+const fotoDePerf = document.getElementById('fotoPerfil');
+fotoDePerf.src = getUserPic();
+
+document.title = "Perfil - " + getUserName();
 
 
 cerrarSesionBtn.onclick = () =>{
@@ -7,3 +13,19 @@ cerrarSesionBtn.onclick = () =>{
     sessionStorage.removeItem('signedUser');
     location.reload();
 };
+
+function getUserPic(){
+    for (let x = 0; x < usuariosArray2.length; x++) {
+        if (usuariosArray2[x].usuario == sessionStorage.getItem("signedUser")){
+            return usuariosArray2[x].fotoPerfil;
+        } 
+    }
+}
+
+function getUserName(){
+    for (let x = 0; x < usuariosArray2.length; x++) {
+        if (usuariosArray2[x].usuario == sessionStorage.getItem("signedUser")){
+            return usuariosArray2[x].nombre;
+        } 
+    }
+}
