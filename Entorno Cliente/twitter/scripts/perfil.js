1143fe661bbdd45bcc92853cc06f4355a39f339d
2AW3 User 2 <2AW3-2@yourdomain.com>
2024-10-24 11:20:05 +0200

    Twitter casi acabado, cambio de foto de perfil hecho y falta poder borrar los tweets

--- Entorno Cliente/twitter/scripts/perfil.js
+++ Entorno Cliente/twitter/scripts/perfil.js
@@ -4,9 +4,13 @@ const GuardarCambios = document.getElementById('btnGuardar');
 const $nombre = document.getElementById('nombre');
 const $usuario = document.getElementById('usuario');
 const $passwd = document.getElementById('passwd');
+const $profilePic = document.getElementById('profilePic');
+const $changePicBtn = document.getElementById('picChange');
 
 const usuariosArray2 = JSON.parse(localStorage.getItem("usuarios"));
 const usuarios = Array();
+
+const tweets = JSON.parse(localStorage.getItem("tweets"));
 
 
 for (let i = 0; i < usuariosArray2.length; i++){
@@ -15,37 +19,93 @@ for (let i = 0; i < usuariosArray2.length; i++){
 
 const fotoDePerf = document.getElementById('fotoPerfil');
 fotoDePerf.src = getUserPic();
+$profilePic.src = getUserPic();
+
+
 
 document.title = "Perfil - " + getUserName();
 
 
-// cerrarSesionBtn.onclick = () =>{
-//     sessionStorage.removeItem('is_Signed');
-//     sessionStorage.removeItem('signedUser');
-//     location.reload();
-// };
+cerrarSesionBtn.onclick = () =>{
+    sessionStorage.removeItem('is_Signed');
+    sessionStorage.removeItem('signedUser');
+    location.reload();
+};
 
 $nombre.value = getUserName();
 $usuario.value = getUsername();
 $passwd.value = getUserPasswd();
 
+let tweetsIdUser = Array();
+
 GuardarCambios.addEventListener('click', () =>{
 
-    
-
+    //cambiar datos en el array de usuarios
     for (let i = 0; i < usuarios.length; i++){
         if (usuarios[i].usuario == sessionStorage.getItem("signedUser")){
             usuarios[i].nombre = $nombre.value;
             usuarios[i].usuario = $usuario.value;
             usuarios[i].password = $passwd.value;
+
+            tweetsIdUser = usuarios[i].tweets;
             
             sessionStorage.setItem("signedUser", usuarios[i].usuario);
-
             localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            break;
         }
     }
+
+    //cambiar datos en los tweets del usuario
+    for (let x = 0; x < tweets.length; x++){
+        for (let k = 0; k < tweetsIdUser.length; k++){
+            if (tweets[x].ID == tweetsIdUser[k]){
+                tweets[x].NOMBRE = $nombre.value;
+                tweets[x].NOMBREUSUARIO = $usuario.value;
+            }
+        }
+    }
+
+    localStorage.setItem("tweets", JSON.stringify(tweets));
     
-})
+});
+
+
+$changePicBtn.addEventListener('change', () =>{
+
+      var fileReader = new FileReader();
+
+      fileReader.onload = function(fileLoadedEvent) {
+        var srcData = fileLoadedEvent.target.result; // <--- data: base64
+        $profilePic.src = srcData;
+        fotoDePerf.src = srcData;
+
+        //cambiar datos en el array de usuarios
+        for (let i = 0; i < usuarios.length; i++){
+            if (usuarios[i].usuario == sessionStorage.getItem("signedUser")){
+                usuarios[i].fotoPerfil = $profilePic.src;
+
+                tweetsIdUser = usuarios[i].tweets;
+                
+                localStorage.setItem("usuarios", JSON.stringify(usuarios));
+                break;
+            }
+        }
+
+        //cambiar datos en los tweets del usuario
+        for (let x = 0; x < tweets.length; x++){
+            for (let k = 0; k < tweetsIdUser.length; k++){
+                if (tweets[x].ID == tweetsIdUser[k]){
+                    tweets[x].IMG = $profilePic.src;                
+                }
+            }
+        }
+
+        localStorage.setItem("tweets", JSON.stringify(tweets));
+      }
+      fileReader.readAsDataURL($changePicBtn.files[0]);  
+
+    
+});
 
 
 function getUserPic(){
